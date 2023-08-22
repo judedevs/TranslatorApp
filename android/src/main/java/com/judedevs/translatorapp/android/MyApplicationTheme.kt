bb0c089edea5feb7f2e4b109dd2951a833681daa
judedevs <hey@judedevs.com>
2023-08-22 18:52:05 +0200

    Sets up shared colours and Android theme.

--- android/src/main/java/com/judedevs/translatorapp/android/MyApplicationTheme.kt
+++ android/src/main/java/com/judedevs/translatorapp/android/MyApplicationTheme.kt
@@ -5,39 +5,56 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Shapes
 import androidx.compose.material.Typography
-import androidx.compose.material.darkColors
-import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.judedevs.translatorapp.android.core.theme.darkThemeColors
+import com.judedevs.translatorapp.android.core.theme.lightThemeColors
 
 @Composable
-fun MyApplicationTheme(
+fun TranslatorTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     content: @Composable () -> Unit
 ) {
     val colors = if (darkTheme) {
-        darkColors(
-            primary = Color(0xFFBB86FC),
-            primaryVariant = Color(0xFF3700B3),
-            secondary = Color(0xFF03DAC5)
-        )
+        darkThemeColors
     } else {
-        lightColors(
-            primary = Color(0xFF6200EE),
-            primaryVariant = Color(0xFF3700B3),
-            secondary = Color(0xFF03DAC5)
-        )
+        lightThemeColors
     }
+    val sfProText = FontFamily(
+        Font(R.font.sf_pro_text_regular, FontWeight.Normal),
+        Font(R.font.sf_pro_text_medium, FontWeight.Medium),
+        Font(R.font.sf_pro_text_bold, FontWeight.Bold)
+    )
     val typography = Typography(
+        h1 = TextStyle(
+            fontFamily = sfProText,
+            fontWeight = FontWeight.Bold,
+            fontSize = 30.sp
+        ),
+        h2 = TextStyle(
+            fontFamily = sfProText,
+            fontWeight = FontWeight.Bold,
+            fontSize = 24.sp
+        ),
+        h3 = TextStyle(
+            fontFamily = sfProText,
+            fontWeight = FontWeight.Medium,
+            fontSize = 18.sp
+        ),
         body1 = TextStyle(
-            fontFamily = FontFamily.Default,
+            fontFamily = sfProText,
             fontWeight = FontWeight.Normal,
-            fontSize = 16.sp
+            fontSize = 14.sp
+        ),
+        body2 = TextStyle(
+            fontFamily = sfProText,
+            fontWeight = FontWeight.Normal,
+            fontSize = 12.sp
         )
     )
     val shapes = Shapes(
